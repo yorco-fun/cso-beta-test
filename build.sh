8a64e5f48acf4aed9f8147dba6f03ed70dad2faf
yorco-fun <71259644+yorco-fun@users.noreply.github.com>
2024-10-26 22:35:58 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
-fi
+# if [[ $CREATE_SUPERUSER ]];
+# then
+#   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+# fi
